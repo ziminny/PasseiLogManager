67e5a526a449afb48b8209c9a8480ecb3495bc1a
Vagner Oliveira <ziminny@gmail.com>
2023-09-05 17:30:07 -0300

    initial configs

---
+++ PasseiLogManager/Classes/constants.m
@@ -0,0 +1,15 @@
+//
+//  test22.m
+//  
+//
+//  Created by Vagner Oliveira on 01/06/23.
+//
+
+#import <Foundation/Foundation.h>
+ 
+#import "constants.h"
+
+@implementation Constants
+
+ 
+@end
---
+++ PasseiLogManager/Classes/include/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  
+//
+//  Created by Vagner Oliveira on 01/06/23.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "../log_manager.h"
+#import "../constants.h"
+
+#endif /* Header_h */
