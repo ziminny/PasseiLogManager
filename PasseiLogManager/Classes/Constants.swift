a9b6dc17f4b8b20fa4dd7c6cc13b109869c992b0
Vagner Oliveira <ziminny@gmail.com>
2023-11-08 22:08:21 -0300

    no message

--- PasseiLogManager/Classes/Constants.swift
+++ PasseiLogManager/Classes/Constants.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Vagner Oliveira on 05/09/23.
 //
@@ -8,5 +8,20 @@
 import Foundation
 
 class Constants {
-    static let PATH_LOG_DEBUG = "/tmp/passei_oab_debug"
+    
+    private static var path = "/tmp/"
+    
+    static var PATH_LOG_DEBUG:String { "\(path)\(productName).txt" }
+    
+    static private func getBundleIdentifier() -> String? {
+        let bundleIdentifier = Bundle.main.bundleIdentifier
+        return bundleIdentifier;
+    }
+
+    static private var productName:String {
+        let bundle = Bundle.main
+        let info = bundle.infoDictionary;
+        let prodName = ((info?["CFBundleName"] as? String) ?? "passei_debug").replacingOccurrences(of: " ", with: "")
+        return prodName;
+    }
 }
