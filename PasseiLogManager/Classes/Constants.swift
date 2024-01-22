a85dc675b538b97d0022989ed6dda3f88831d6f0
Vagner Oliveira <ziminny@gmail.com>
2024-01-21 21:57:59 -0300

    Documentation

--- PasseiLogManager/Classes/Constants.swift
+++ PasseiLogManager/Classes/Constants.swift
@@ -7,21 +7,33 @@
 
 import Foundation
 
+/// Contém constantes utilizadas no modulo PasseiLogManager.
 class Constants {
     
+    /// Caminho base para arquivos de log.
     private static var path = "/tmp/"
     
-    static var PATH_LOG_DEBUG:String { "\(path)\(productName).txt" }
+    /// Caminho completo para o arquivo de log de depuração.
+    ///
+    /// O caminho é construído usando o diretório temporário (`path`) e o nome do produto (`productName`).
+    static internal var PATH_LOG_DEBUG: String { "\(path)\(productName).txt" }
     
+    /// Obtém o identificador de pacote da aplicação.
+    ///
+    /// - Returns: O identificador de pacote da aplicação, ou `nil` se não estiver disponível.
     static private func getBundleIdentifier() -> String? {
         let bundleIdentifier = Bundle.main.bundleIdentifier
-        return bundleIdentifier;
+        return bundleIdentifier
     }
 
-    static private var productName:String {
+    /// Obtém o nome do produto da aplicação, removendo espaços em branco.
+    ///
+    /// - Returns: O nome do produto da aplicação.
+    static private var productName: String {
         let bundle = Bundle.main
-        let info = bundle.infoDictionary;
+        let info = bundle.infoDictionary
         let prodName = ((info?["CFBundleName"] as? String) ?? "passei_debug").replacingOccurrences(of: " ", with: "")
-        return prodName;
+        return prodName
     }
 }
+
--- PasseiLogManager/Classes/LogManager.swift
+++ PasseiLogManager/Classes/LogManager.swift
@@ -9,14 +9,23 @@ import Foundation
 import OSLog
 
 @available(iOS 13.4, *)
+/// Gerencia o registro de logs da aplicação.
 public class LogManager {
     
-    private static var filePath:String = Constants.PATH_LOG_DEBUG
-    private static var fileManager:FileManager = .default
-    public static func dispachLog(_ message:String) {
+    /// Caminho do arquivo de log de depuração.
+    private static var filePath: String = Constants.PATH_LOG_DEBUG
+    
+    /// Gerenciador de arquivo padrão.
+    private static var fileManager: FileManager = .default
+    
+    /// Registra uma mensagem no arquivo de log ou na console, dependendo do ambiente de execução.
+    ///
+    /// - Parameter message: A mensagem a ser registrada.
+    public static func dispachLog(_ message: String) {
         
         #if targetEnvironment(simulator)
         
+        // Se estiver no simulador, registra a mensagem em um arquivo de log no diretório temporário.
         if !fileManager.fileExists(atPath: filePath) {
             if fileManager.createFile(atPath: filePath, contents: nil, attributes: [FileAttributeKey.posixPermissions: 0o644]) {
                 // Arquivo criado com sucesso
@@ -33,12 +42,19 @@ public class LogManager {
         }
             
         #else
+        // Se não estiver no simulador, usa o sistema de logs do iOS.
         os_log("%@", log: .customCategory, type: .info, message as CVarArg)
         #endif
     }
 }
 
+/// Extensão para definir uma categoria personalizada para os logs.
 fileprivate extension OSLog {
-    private static var subsystem:String = Bundle.main.bundleIdentifier!
-    static let customCategory:OSLog = OSLog(subsystem: subsystem, category: "Passei-group")
+    
+    /// Subsistema utilizado para os logs.
+    private static var subsystem: String = Bundle.main.bundleIdentifier!
+    
+    /// Categoria personalizada para os logs.
+    static let customCategory: OSLog = OSLog(subsystem: subsystem, category: "Passei-group")
 }
+
